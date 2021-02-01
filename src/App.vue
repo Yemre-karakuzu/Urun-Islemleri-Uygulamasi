35880e06fb93941dc14f64731fc238e38f3a1701
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-02-01 23:32:59 +0300

    added:components

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header></Header>
+    <!-- <ProductPurchase></ProductPurchase> -->
+    <!-- <ProductSell></ProductSell> -->
+    <ProductList></ProductList>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+//import ProductPurchase from "./components/products/ProductPurchase";
+//import ProductSell from "./components/products/ProductSell";
+import ProductList from "./components/products/ProductList";
+export default {
+  components: {
+    Header,
+    Footer,
+    //ProductPurchase,
+    //ProductSell,
+    ProductList,
+  },
+};
+</script>
+
+<style>
+</style>
