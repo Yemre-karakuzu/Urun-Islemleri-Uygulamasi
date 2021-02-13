ef930fe40b857f4588522e25b6a0dad42420294b
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-02-14 01:59:02 +0300

    added:vue resource

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from "./router"
 import { store } from "./store/store"
+import vueResource from "vue-resource"
+import VueResource from 'vue-resource'
 
-
-
+Vue.use(VueResource)
 new Vue({
   router,
   store,
+  // vueResource,
   render: h => h(App),
 }).$mount('#app')
